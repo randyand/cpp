563d93f97251b30ae5e0eac5c6d3e88db549c142
Randy Gavarrete <randygavarrete7@gmail.com>
2022-06-20 23:35:54 -0600

    ejercicio2

---
+++ ejercicio2hola mundo con nombre/mensajes.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    string nombre;
+
+    cout << "Ingrese su nombre: ";
+    cin >> nombre ;
+    cout<< "Hola mi nombre es " << nombre<< endl;
+    return 0;
+}
