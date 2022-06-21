3b08f7af507c4a6f980b96cfd1000ef01a479eb7
Randy Gavarrete <randygavarrete7@gmail.com>
2022-06-21 00:25:30 -0600

    Ejercicio suma

---
+++ Ejercicio5-Programa suma/suma.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a = 0;
+    int b = 0;
+    int resultado = 0;
+
+    cout << "Ingrese el valor de a: ";
+    cin>> a;
+    cout << "Ingrese el valor de b: ";
+    cin>> b;
+
+    resultado = a + b ;
+
+    cout << "la suma de  a + b es: " << resultado;
+
+    return 0;
+}
