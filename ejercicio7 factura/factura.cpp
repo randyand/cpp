5a73afa4df8a33617cbeeffd7f1247690d478e04
Randy Gavarrete <randygavarrete7@gmail.com>
2022-06-21 21:45:23 -0600

    ejercicio factura

--- ejercicio7 factura/factura.cpp
+++ ejercicio7 factura/factura.cpp
@@ -5,19 +5,14 @@ int main(int argc, char const *argv[])
     double subtotal;
     double total;
     double impuesto= 0.15;
-    int descuento = 0;
     double calculoDescuento;
     double calculoImpuesto;
 
     cout<< "Ingrese subtotal de la factura: ";
     cin>> subtotal;
 
-    cout<< "Ingrese descuento (0,10,15,20): ";
-    cin>> descuento;
-
-    calculoDescuento= (subtotal*descuento)/100;
-    calculoImpuesto = (subtotal- calculoDescuento)*impuesto;
-    total=  subtotal - calculoDescuento + calculoImpuesto;
+    calculoImpuesto = subtotal*impuesto;
+    total=  subtotal + calculoImpuesto;
 
      cout<< "El total a pagar es: " << total;
 
