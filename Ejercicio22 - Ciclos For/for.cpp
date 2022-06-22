211009dd14509854272ce35bd15ce91174f74c92
Randy Gavarrete <randygavarrete7@gmail.com>
2022-06-22 15:14:03 -0600

    ciclo for

---
+++ Ejercicio22 - Ciclos For/for.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    for (int indice= 0; indice <= 10; indice++)
+    {
+        cout<< "Valor de la variable indice "<<indice <<endl;
+    }
+    
+    return 0;
+}
+
+
