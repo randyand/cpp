a390bf08a63baa2d154d56b5a64316ba762c787e
Randy Gavarrete <randygavarrete7@gmail.com>
2022-07-11 20:21:37 -0600

    ejercicio 32 throw

---
+++ Ejercicio-32_calculadora con funciones -trwon/main.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+using namespace std;
+
+int sumar(int a , int b)
+{
+    return a+b;
+}
+
+int restar(int a , int b)
+{
+    return a-b;
+}
+
+int multiplicar(int a , int b)
+{
+    return a*b;
+}
+
+int dividir(int a , int b)
+{
+    return a/b;
+}
+
+int calculadora(int a , int b , char operador)
+{
+    if (operador == '+'){
+        return sumar(a,b);
+    }
+    if (operador == '-'){
+        return restar(a,b);
+    }
+    if (operador == '*'){
+        return multiplicar(a,b);
+    }
+    if (operador == '/'){
+        return dividir(a,b);
+    }
+    throw"El operador no existe.";
+}
+
+int main(int argc, char const *argv[])
+{
+    int n1=0 , n2= 0;
+    char operador;
+
+    cout<< "Ingrese a: ";
+    cin>>n1;
+    
+    cout<< "Ingrese a: ";
+    cin>>n2;
+
+    cout<< "Ingrese el operador (+ , - , * , / ) : ";
+    cin>>operador;
+
+    
+    try
+    {
+        cout<< "El resultado de " << n1 <<" "<< operador<< " "<< n2 <<" "<<"es :";
+        cout<<calculadora(n1, n2, operador);
+    }
+    catch(const char* error)
+    {
+        cout << error;
+    }
+    
+    return 0;
+}
