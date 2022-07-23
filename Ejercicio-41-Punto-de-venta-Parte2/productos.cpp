bbb11cfd365e1b794692f3e76b14111d669ff1bb
Randy Gavarrete <randygavarrete7@gmail.com>
2022-07-22 21:39:55 -0600

    parte 2

---
+++ Ejercicio-41-Punto-de-venta-Parte2/productos.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+void productos(int opcion){
+    system("cls");
+    int opcionProducto =0;
+    switch (opcion)
+				 {
+        case 1:{
+            cout<< "Bebidas Calientes." <<endl;
+            cout<< "******************" <<endl;
+            system("pause");
+            break;
+            
+            } 
+        case 2:{
+            cout<< "Bebidas Frias." <<endl;
+            cout<< "******************" <<endl;
+            system("pause");
+            break;
+            }
+        case 3:	{
+            cout<< "Reposteria." <<endl;
+            cout<< "**********" <<endl;
+            system("pause");
+            break;             
+                }
+        default:{
+            break;
+        }        
+    }
+}                
