05745ecf038b3ae5ce3f2226d6341aa6033b845b
Randy Gavarrete <randygavarrete7@gmail.com>
2022-07-22 23:26:02 -0600

    ejercicio 43

---
+++ Ejercicio-43-Punto-de-venta- parte4/productos.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+
+extern void agregarproducto(string descripcion,int cantidad,double precio);
+void productos(int opcion){				
+    system("cls");
+    int opcionProducto =0;
+    switch (opcion)
+				 {
+        case 1:{
+            cout<< "Bebidas Calientes." <<endl;
+            cout<< "******************" <<endl;
+            cout<< "1- Capuccino." <<endl;
+            cout<< "2- Expresso." <<endl;
+            cin>> opcionProducto;
+
+
+                switch (opcionProducto)	{
+				    case 1:{
+                        agregarproducto("1 Capuccino",1,40);
+                        cout<< endl; 
+					    break;
+    				        }
+             	    case 2:{
+                        agregarproducto("1 Expreso",1,30);
+                        cout<< endl; 
+                        break;
+           				    }
+            
+                    default:{
+                        cout<< "Opcion no valida"<<endl;
+				        break;
+            		        }	
+                break;
+       	        }
+            cout<< endl; 
+            cout<< "producto agregado"<<endl;						
+            
+            break;
+		    }
+
+                    case 2:	{
+		                cout<< "Bebidas frias." <<endl;
+                        cout<< "******************" <<endl;
+                        system("pause");
+
+
+                    break;
+                    }
+
+                         case 3:	{
+                                cout<< "Reposteria." <<endl;
+                                cout<< "**********" <<endl;
+                                system("pause");
+                     break;
+				    }	
+                    break;	
+                 }
+                 return	;
+		    }
