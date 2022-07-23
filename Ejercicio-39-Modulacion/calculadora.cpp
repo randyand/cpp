2693a289e6d8032c2a8de031f22b11ad139753b0
Randy Gavarrete <randygavarrete7@gmail.com>
2022-07-22 20:48:43 -0600

    modulacion

---
+++ Ejercicio-39-Modulacion/calculadora.cpp
@@ -0,0 +1,5 @@
+
+int sumar( int a , int b){
+    return a + b;
+}
+
---
+++ Ejercicio-39-Modulacion/main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+extern int sumar( int a , int b);
+int main(int argc, char const *argv[])
+{
+    system("cls");
+    cout<< sumar(5,7);
+    cout<< endl;
+    return 0;
+}
